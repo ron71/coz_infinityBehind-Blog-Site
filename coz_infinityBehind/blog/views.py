8c3d3310a343aae0a50ffd28985e78bcfc31f98c
Captain <maxphinix@gmail.com>
2018-10-17 08:48:00 +0530

    coz_infinityBehind- First Commit

--- coz_infinityBehind/blog/views.py
+++ coz_infinityBehind/blog/views.py
@@ -5,7 +5,7 @@ from blog.models import Post
 
 
 def blog(request):
-    blog_list = Post.objects.order_by('date')
+    blog_list = Post.objects.order_by('-date')
     blog_dict = {"blog_records": blog_list,}
 
     return render(request, "blog/blog.html", context=blog_dict)
